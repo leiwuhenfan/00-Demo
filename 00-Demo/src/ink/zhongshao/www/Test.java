7d61222b556e7f151d9c19c0ecfb79b41af23133
leiwuhenfan <wf@wf-PC>
2019-04-24 09:35:48 +0800

    从暂存区(index/Stage)提交到本地资源库（D:\Study\download\git\）

---
+++ 00-Demo/src/ink/zhongshao/www/Test.java
@@ -0,0 +1,53 @@
+package ink.zhongshao.www;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * “|”运算符：不论运算符左侧为true还是false，右侧语句都会进行判断
+	 */
+	public static void test1() {
+		int a = 1, b = 1;
+		if (a++ == 1 | ++b == 2)
+			System.out.println("true");
+		System.out.println("a= " + a + "  ,b=  " + b);
+	}
+
+	/**
+	 * “||”运算符：若运算符左边为true，则不再对运算符右侧进行运算
+	 */
+	public static void test2() {
+		int a = 1, b = 1;
+		if (a++ == 1 || ++b == 2)
+			System.out.println("true");
+		System.out.println("a= " + a + "  ,b=  " + b);
+	}
+
+	/**
+	 * &运算符与|运算符类似：不论运算符左侧为true还是false，右侧语句都会进行判断
+	 */
+	public static void test3() {
+		int a = 1, b = 1;
+		if (a++ == 2 & ++b == 2)
+			System.out.println(true);
+		else
+			System.out.println(false);
+		System.out.println("a= " + a + "  ,b=  " + b);
+	}
+
+	/**
+	 * “&&”运算符与“||”运算符类似：若运算符左侧为false则不再对右侧语句进行判断
+	 */
+	public static void test4() {
+		int a = 1, b = 1;
+		if (a++ == 2 && ++b == 2)
+			System.out.println(true);
+		else
+			System.out.println(false);
+		System.out.println("a= " + a + "  ,b=  " + b);
+	}
+
+}
